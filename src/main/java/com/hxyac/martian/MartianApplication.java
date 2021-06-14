91e1c3b0976d5a0a8980ecd2030a8b893719b43e
Hxy-win10 <zx841985525@163.com>
2021-06-14 22:07:23 +0800

    feat: 🎸 init martain
    init martain,begin my new blog!!!!
    
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: no

---
+++ src/main/java/com/hxyac/martian/MartianApplication.java
@@ -0,0 +1,14 @@
+package com.hxyac.martian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("top.hxyac.blogshop.mapper")
+public class MartianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hxyac/martian/MartianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxyac.martian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
